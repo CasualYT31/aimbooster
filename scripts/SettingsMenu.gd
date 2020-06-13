0fa6be13c147ff81ab9b017b8c25d67e053858ba
maskedforgas <shockedelephant314@outlook.com>
2020-06-13 11:41:34 -0500

    Added functionality to Time and Lives sliders.

--- scripts/SettingsMenu.gd
+++ scripts/SettingsMenu.gd
@@ -6,6 +6,10 @@ func _ready():
 	get_node("MarginContainer/VBoxContainer/GridContainer/SoundValueLabel").text = str(Settings.soundVolume)
 	get_node("MarginContainer/VBoxContainer/GridContainer/MusicVolume").value = Settings.musicVolume
 	get_node("MarginContainer/VBoxContainer/GridContainer/MusicValueLabel").text = str(Settings.musicVolume)
+	#Lives
+	#LivesValueLabel
+	get_node("MarginContainer/VBoxContainer/GridContainer/Time").value = Settings.time
+	get_node("MarginContainer/VBoxContainer/GridContainer/TimeValueLabel").text = str(Settings.time) + " Mins"
 
 func _on_BackButton_pressed():
 	Settings.currentMenu = Settings.previousMenu
@@ -18,6 +22,15 @@ func _on_MusicVolume_value_changed(value):
 	Settings.musicVolume = value
 	get_node("MarginContainer/VBoxContainer/GridContainer/MusicValueLabel").text = str(value)
 
-
 func _on_Fullscreen_toggled(button_pressed):
 	Settings.fullscreen = !Settings.fullscreen
+
+
+func _on_Lives_value_changed(value):
+	Settings.lives = value
+	get_node("MarginContainer/VBoxContainer/GridContainer/LivesValueLabel").text = str(value)
+
+
+func _on_Time_value_changed(value):
+	Settings.time = value
+	get_node("MarginContainer/VBoxContainer/GridContainer/TimeValueLabel").text = str(value) + " Mins"
