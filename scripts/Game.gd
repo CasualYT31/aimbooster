906abce94c33cd8b8dbc1999a79272b9ed267fe3
CasualYT31 <21147925+CasualYT31@users.noreply.github.com>
2020-07-04 12:46:20 +0100

    Completed HUD

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -5,25 +5,34 @@ var Settings = preload("res://scripts/Settings.gd")
 
 var statistics
 # keeps track of the length of the game, not including pausing
-var currentTime: int = OS.get_unix_time()
+var startTime: int = OS.get_unix_time()
+var currentTime: int
+var lives: int
 
 var settingsMenu
 
 func _isPaused():
-	return get_node("PauseMenu").visible
+	return get_node("GameGUI/PauseMenu").visible
 
 func _pause():
-	get_node("PauseMenu").visible = true
+	get_node("GameGUI/PauseMenu").visible = true
 	get_node("SecondCounter").stop()
 
 func _unpause():
-	get_node("PauseMenu").visible = false
+	get_node("GameGUI/PauseMenu").visible = false
 	get_node("SecondCounter").start()
+
+func _updateLivesDisplay():
+	get_node("GameGUI/HUD/LivesLabel").text = "Lives: " + (str(lives) if lives > 0 else "Inf")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var settings = Settings.new()
 	statistics = Statistics.new(settings.lives, settings.time)
+	currentTime = startTime - 1
+	_on_SecondCounter_timeout()
+	lives = settings.lives
+	_updateLivesDisplay()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -38,11 +47,17 @@ func _unhandled_input(event):
 				_pause()
 
 func _makeAllVisible(flag):
-	get_node("PauseMenu").visible = flag
+	get_node("GameGUI/PauseMenu").visible = flag
 
 func _on_SecondCounter_timeout():
 	currentTime += 1
-	get_node("Label").text = str(currentTime)
+	var minutes: String = str((currentTime - startTime) / 60)
+	if len(minutes) == 1:
+		minutes = "0" + minutes
+	var seconds: String = str((currentTime - startTime) % 60)
+	if len(seconds) == 1:
+		seconds = "0" + seconds
+	get_node("GameGUI/HUD/TimeLabel").text = "Time: " + minutes + ":" + seconds
 
 func _on_ContinueButton_pressed():
 	_unpause()
