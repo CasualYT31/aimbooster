a95159af20fd607199159767fe56f3f95d60056b
maskedforgas <shockedelephant314@outlook.com>
2020-07-06 18:57:44 -0500

    Removed hit detection method, as it failed to work correctly

--- scripts/Target.gd
+++ scripts/Target.gd
@@ -1,8 +1,6 @@
-extends Sprite
+extends Area2D
 
 var Settings = preload("res://scripts/Settings.gd")
-
-var health: int
 var leftToShoot: bool
 var buttonToShoot
 
@@ -17,42 +15,44 @@ func _ready():
 	if settings.isLeftButtonToShoot == true:
 		buttonToShoot = BUTTON_LEFT
 	else:
-		buttonToShoot = BUTTON_RIGHT 
+		buttonToShoot = BUTTON_RIGHT
 
 func getType():
 	return type
-
+	
+	
+# warning-ignore:unused_argument
+# warning-ignore:unused_argument
 func _init(targetType: int, health: int, x: float, y: float):
 	match targetType:
 		Type.RED:
 			modulate = ColorN("red", 1.0)
+# warning-ignore:unused_variable
 			var targetHealth: int = health
 		Type.ORANGE:
 			modulate = ColorN("orange", 1.0)
 			scale = Vector2(0.9, 0.9)
+# warning-ignore:unused_variable
 			var targetHealth: int = health
 		Type.YELLOW:
 			modulate = ColorN("yellow", 1.0)
 			scale = Vector2(0.8, 0.8)
+# warning-ignore:unused_variable
 			var targetHealth: int = health
 		Type.GREEN:
 			modulate = ColorN("green", 1.0)
 			scale = Vector2(0.7, 0.7)
+# warning-ignore:unused_variable
 			var targetHealth: int = health
 		Type.BLUE:
 			modulate = ColorN("blue", 1.0)
 			scale = Vector2(0.6, 0.6)
+# warning-ignore:unused_variable
 			var targetHealth: int = health
 		Type.PURPLE:
 			modulate = ColorN("purple", 1.0)
 			scale = Vector2(0.5, 0.5)
+# warning-ignore:unused_variable
 			var targetHealth: int = health
 		_:
 			OS.alert("Invalid target type!")
-
-# on click within defined area, it will print "Clicked"
-func _on_Area2D_input_event(viewport, event, shape_idx, targetHealth: int):
-	if event.type == InputEvent.MOUSE_BUTTON \
-	and event.button_index == buttonToShoot \
-	and event.pressed:
-		targetHealth-=1
