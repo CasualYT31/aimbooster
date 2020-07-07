a191f60a32552ddc89b310380e0c28aceae8a4d0
CasualYT31 <21147925+CasualYT31@users.noreply.github.com>
2020-07-07 18:34:45 +0100

    A lot of changes: expanding Target.gd and began work on target spawn management

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -2,16 +2,42 @@ extends Control
 
 var Statistics = preload("res://scripts/Statistics.gd")
 var Settings = preload("res://scripts/Settings.gd")
+var Target = preload("res://scenes/Target.tscn")
 
 var statistics
 var settings
+var settingsMenu
+
+var lives: int = 0
+
 # keeps track of the length of the game, not including pausing
 var seconds: float = 0.0
 var minutes: int = 0
-var spawnTimerVal: int
-var lives: int
 
-var settingsMenu
+# spawning timing variables
+var spawnTimerCounter: float = 0.0
+var timeUntilNextSpawn: float = 3.25
+# variables that define difficulty
+# chance variables will be a %age from 0-100
+# they represent a chance of a property being a certain way
+# i.e. "50% chance of target being red,
+#       45% chance of it being orange,
+#       5% chance of it being yellow,
+#       0% chance of it being green, etc."
+# each group of chance variables should add up to 100
+# this is the target type group
+var chanceOfRedTarget: int = 100
+var chanceOfOrangeTarget: int = 0
+var chanceOfYellowTarget: int = 0
+var chanceOfGreenTarget: int = 0
+var chanceOfBlueTarget: int = 0
+var chanceOfPurpleTarget: int = 0
+# chance of a target being stationary
+# if not stationary, position will be totally random yet confined within the screen
+var chanceOfStationaryTarget: int = 100
+# how long the target should remain on screen for
+# this value is to be gradually made smaller when adjusting difficulty
+var activeLifeOfTarget: float = 3.0
 
 func _isPaused():
 	return get_node("GameGUI/PauseMenu").visible
@@ -24,6 +50,18 @@ func _unpause():
 
 func _updateLivesDisplay():
 	get_node("GameGUI/HUD/LivesLabel").text = "Lives: " + (str(lives) if lives > 0 else "Inf")
+
+func _updateTimeDisplay():
+	var strSeconds = str(int(seconds))
+	if int(seconds) == 60:
+		seconds = 0.0
+		minutes += 1
+	var strMinutes = str(minutes)
+	if len(strMinutes) == 1:
+		strMinutes = "0" + strMinutes
+	if len(strSeconds) == 1:
+		strSeconds = "0" + strSeconds
+	get_node("GameGUI/HUD/TimeLabel").text = "Time: " + strMinutes + ":" + strSeconds
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -44,19 +82,73 @@ func _makeAllVisible(flag):
 	get_node("GameGUI").visible = flag
 
 func _process(delta):
-	# HUD timer
+	_incrementTimeCounters(delta)
+	_updateTimeDisplay()
+	_targetSpawnManager()
+
+func _targetSpawnManager():
+	if spawnTimerCounter >= timeUntilNextSpawn:
+		# spawn a target here!
+		# we should design our code so that we don't have to explicitly store references to targets
+		var targetType: int = _generateNewTargetType()
+		var targetHealth: int = _generateNewTargetHealth(targetType)
+		var targetStartPosition: Vector2 = _generateNewTargetPosition()
+		var targetEndPosition: Vector2
+		if _newTargetShouldBeAnimate():
+			targetEndPosition = targetStartPosition
+		else:
+			targetEndPosition = _generateNewTargetPosition()
+		var newTarget = Target.new(targetType, targetHealth, targetStartPosition, targetEndPosition, activeLifeOfTarget)
+		get_node("TargetParent").add_child(newTarget)
+		
+		# probably connect a signal here which is sent by Target when it is killed
+		spawnTimerCounter = 0.0
+		_increaseDifficulty()
+
+func _increaseDifficulty():
+	# adjust the following variables:
+	# target type chance group - ensure they all add up to 100!!
+	# target stationary chance
+	# decrease both activeLifeOfTarget and timeUntilNextSpawn by a small random amount
+	pass
+
+func _generateNewTargetType():
+	var random: int = randi() % 100 + 1 # random number between 1 and 100
+	if random <= chanceOfRedTarget:
+		return Target.Type.RED
+	elif random <= chanceOfRedTarget + chanceOfOrangeTarget:
+		return Target.Type.ORANGE
+	elif random <= chanceOfRedTarget + chanceOfOrangeTarget + chanceOfYellowTarget:
+		return Target.Type.YELLOW
+	elif random <= chanceOfRedTarget + chanceOfOrangeTarget + chanceOfYellowTarget + chanceOfGreenTarget:
+		return Target.Type.GREEN
+	elif random <= chanceOfRedTarget + chanceOfOrangeTarget + chanceOfYellowTarget + chanceOfGreenTarget + chanceOfBlueTarget:
+		return Target.Type.BLUE
+	else:
+		return Target.Type.PURPLE
+
+func _generateNewTargetHealth(targetType: int):
+	if settings.isEnemyMode:
+		match targetType:
+			Target.Type.RED, Target.Type.ORANGE:
+				return 2
+			Target.Type.YELLOW, Target.Type.GREEN:
+				return 3
+			Target.Type.BLUE, Target.Type.PURPLE:
+				return 4
+	else:
+		return 1
+
+func _generateNewTargetPosition():
+	return Vector2(randi() % int(OS.get_window_size().x), randi() % int(OS.get_window_size().y))
+
+func _newTargetShouldBeAnimate():
+	return (randi() % 100 + 1) <= chanceOfStationaryTarget
+
+func _incrementTimeCounters(delta: float):
 	if !_isPaused():
 		seconds += delta
-	var strSeconds = str(int(seconds))
-	if int(seconds) == 60:
-		seconds = 0.0
-		minutes += 1
-	var strMinutes = str(minutes)
-	if len(strMinutes) == 1:
-		strMinutes = "0" + strMinutes
-	if len(strSeconds) == 1:
-		strSeconds = "0" + strSeconds
-	get_node("GameGUI/HUD/TimeLabel").text = "Time: " + strMinutes + ":" + strSeconds
+		spawnTimerCounter += delta
 
 func _on_ContinueButton_pressed():
 	_unpause()
--- scripts/Target.gd
+++ scripts/Target.gd
@@ -1,12 +1,25 @@
 extends Area2D
 
-var Settings = preload("res://scripts/Settings.gd")
+# these signals are emitted when the target has been either hit or missed
+# in both cases, Target.gd deals with the actual removal of the target object
+# so other tasks that need to be carried out by Game.gd should be done there,
+# using these signals to identify when they need to be carried out
+signal target_hit
+signal target_miss
+
 var leftToShoot: bool
 var buttonToShoot
 
+var Settings = preload("res://scripts/Settings.gd")
+
 enum Type {RED, ORANGE, YELLOW, GREEN, BLUE, PURPLE}
 
-var type: int setget , getType
+export var type: int setget , getType
+export var startPosition: Vector2 setget , getStartPosition
+export var endPosition: Vector2 setget , getEndPosition
+# the number of seconds the target remains on the screen for:
+export var activeDuration: float setget , getActiveDuration
+export var targetHealth: int setget , getHealth
 
 func _ready():
 	var settings = Settings.new()
@@ -19,40 +32,51 @@ func _ready():
 
 func getType():
 	return type
-	
-	
-# warning-ignore:unused_argument
-# warning-ignore:unused_argument
-func _init(targetType: int, health: int, x: float, y: float):
+
+func getStartPosition():
+	return startPosition
+
+func getEndPosition():
+	return endPosition
+
+func getActiveDuration():
+	return activeDuration
+
+func getHealth():
+	return targetHealth
+
+# call this method to "hit" the target
+# if the health is at or below 0, it will remove itself
+func hit():
+	targetHealth -= 1
+	if targetHealth <= 0:
+		get_parent().remove_child(self)
+		emit_signal("target_hit")
+
+# constructor
+func _init(targetType: int, health: int, startPos: Vector2, endPos: Vector2, howLongToKeepOnScreen: float):
 	match targetType:
 		Type.RED:
 			modulate = ColorN("red", 1.0)
-# warning-ignore:unused_variable
-			var targetHealth: int = health
+			# no scaling applied to the biggest size
 		Type.ORANGE:
 			modulate = ColorN("orange", 1.0)
 			scale = Vector2(0.9, 0.9)
-# warning-ignore:unused_variable
-			var targetHealth: int = health
 		Type.YELLOW:
 			modulate = ColorN("yellow", 1.0)
 			scale = Vector2(0.8, 0.8)
-# warning-ignore:unused_variable
-			var targetHealth: int = health
 		Type.GREEN:
 			modulate = ColorN("green", 1.0)
 			scale = Vector2(0.7, 0.7)
-# warning-ignore:unused_variable
-			var targetHealth: int = health
 		Type.BLUE:
 			modulate = ColorN("blue", 1.0)
 			scale = Vector2(0.6, 0.6)
-# warning-ignore:unused_variable
-			var targetHealth: int = health
 		Type.PURPLE:
 			modulate = ColorN("purple", 1.0)
 			scale = Vector2(0.5, 0.5)
-# warning-ignore:unused_variable
-			var targetHealth: int = health
 		_:
 			OS.alert("Invalid target type!")
+	targetHealth = health
+	startPosition = startPos
+	endPosition = endPos
+	activeDuration = howLongToKeepOnScreen
