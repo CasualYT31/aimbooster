1ba8aa0b60cf0c5b8aa8d2e38aa3f8798739a12c
CasualYT31 <21147925+CasualYT31@users.noreply.github.com>
2020-07-09 18:03:00 +0100

    Adding bullet hole graphics, destroyed targets now remain visible and stationary for one second before being removed

--- scripts/Target.gd
+++ scripts/Target.gd
@@ -10,6 +10,7 @@ signal target_destroy
 
 var internalTimer: float
 var referenceToSettings # so that the shoot button can be read via the reference and be up to date always
+var timeAtWhichDestroyed: float
 
 export var type: int setget , getType
 export var startPosition: Vector2 setget , getStartPosition
@@ -35,26 +36,28 @@ func getHealth():
 
 # constructor
 func initialiseTarget(settingsReference, targetType: int, health: int, startPos: Vector2, endPos: Vector2, howLongToKeepOnScreen: float):
+	if howLongToKeepOnScreen == 0.0:
+		return # prevent division by 0 error in _process
 	referenceToSettings = settingsReference
 	match targetType:
 		Global.TargetType.RED:
 			modulate = ColorN("red", 1.0)
-			# no scaling applied to the biggest size
+			$Area2D.scale = Vector2(1.5, 1.5)
 		Global.TargetType.ORANGE:
 			modulate = ColorN("orange", 1.0)
-			scale = Vector2(1.4, 1.4)
+			$Area2D.scale = Vector2(1.4, 1.4)
 		Global.TargetType.YELLOW:
 			modulate = ColorN("yellow", 1.0)
-			scale = Vector2(1.3, 1.3)
+			$Area2D.scale = Vector2(1.3, 1.3)
 		Global.TargetType.GREEN:
 			modulate = ColorN("green", 1.0)
-			scale = Vector2(1.2, 1.2)
+			$Area2D.scale = Vector2(1.2, 1.2)
 		Global.TargetType.BLUE:
 			modulate = ColorN("blue", 1.0)
-			scale = Vector2(1.1, 1.1)
+			$Area2D.scale = Vector2(1.1, 1.1)
 		Global.TargetType.PURPLE:
 			modulate = ColorN("purple", 1.0)
-			scale = Vector2(1.0, 1.0)
+			$Area2D.scale = Vector2(1.0, 1.0)
 		_:
 			OS.alert("Invalid target type!")
 	targetHealth = health
@@ -62,25 +65,31 @@ func initialiseTarget(settingsReference, targetType: int, health: int, startPos:
 	activeDuration = howLongToKeepOnScreen
 	startPosition = startPos
 	# manage positions
-	position = startPosition
+	$Area2D.position = startPosition
 	var newCurve := Curve2D.new()
-	newCurve.add_point(Vector2(0, 0))
-	newCurve.add_point(endPosition - startPosition)
-	print(str(newCurve.get_point_position(0)) + " - " + str(newCurve.get_point_position(1)))
+	newCurve.add_point(startPosition)
+	newCurve.add_point(endPosition)
 	set_curve(newCurve)
 
 func _process(delta):
 	internalTimer += delta
-	$Area2D.position = curve.interpolate_baked(internalTimer / activeDuration * curve.get_baked_length(), true)
-	if internalTimer >= activeDuration:
-		get_parent().remove_child(self)
-		emit_signal("target_miss")
+	if targetHealth > 0:
+		$Area2D.position = curve.interpolate_baked(internalTimer / activeDuration * curve.get_baked_length(), true)
+		if internalTimer >= activeDuration:
+			get_parent().remove_child(self)
+			emit_signal("target_miss")
+	else:
+		if internalTimer - timeAtWhichDestroyed >= 1.0:
+			get_parent().remove_child(self)
 
 func _on_Area2D_input_event(_viewport, event, _shape_idx):
-	if event is InputEventMouseButton:
+	if targetHealth > 0 && event is InputEventMouseButton:
 		if event.is_pressed() && event.button_index == (BUTTON_LEFT if referenceToSettings.isLeftButtonToShoot else BUTTON_RIGHT):
 			targetHealth -= 1
+			# move bullet hole graphic and make visible
+			get_node("Area2D/BulletHole" + str(0 if targetHealth < 0 else targetHealth)).offset = (event.position - $Area2D.position) / $Area2D.scale
+			get_node("Area2D/BulletHole" + str(0 if targetHealth < 0 else targetHealth)).visible = true
 			emit_signal("target_hit")
 			if targetHealth <= 0:
-				get_parent().remove_child(self)
+				timeAtWhichDestroyed = internalTimer
 				emit_signal("target_destroy")
